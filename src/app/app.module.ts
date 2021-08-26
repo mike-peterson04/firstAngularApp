efe00d6b34751786c17a6eaf13692ef7bbc94d51
Mike Peterson <mpeterson2004@gmail.com>
2021-08-25 21:16:57 -0600

    deleted practice components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitlePracticeComponent } from './title-practice.component';
-import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitlePracticeComponent,
-    Test2Component
     
   ],
   imports: [
