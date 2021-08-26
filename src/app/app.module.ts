83fc8cf0996d7da77bdd47b22c490840e3e66dbc
Mike Peterson <mpeterson2004@gmail.com>
2021-08-25 21:36:49 -0600

    created planned components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GameComponent,
+    PlayerComponent,
+    ResultsComponent,
     
   ],
   imports: [
